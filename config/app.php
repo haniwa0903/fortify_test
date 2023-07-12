a1d7c02c8dba1564970805749e63c8ac6c6b70e9
haniwa0903 <okumura@i-collabo.com>
2023-07-12 14:15:18 +0900

    user name login

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //追加
+        App\Providers\FortifyServiceProvider::class,
     ])->toArray(),
 
     /*
